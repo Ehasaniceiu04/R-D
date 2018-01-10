23ebea2b977e8106b14344336a1237a6e5925d02
Ehasanul Hoque <ehasan@outlook.com>
2018-01-10 17:04:34 +0600

    adding dynamic project commit

---
+++ RD/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = GetDynamicData(2);
+            Console.WriteLine(test.Name);
+        }
+        public static dynamic GetDynamicData(dynamic i)
+        {
+            dynamic listdata = new[]
+            { new {
+                Id=1,
+                Name = "Ehasanul Hoque",
+                Position = "Senior Software Engineeer"
+            },
+            new {
+                Id=2,
+                Name = "Ehasanul Hoque",
+                Position = "Senior Software Engineeer"
+            }
+            ,
+            new {
+                Id=3,
+                Name = "Ehasanul Hoque",
+                Position = "Senior Software Engineeer"
+            }
+            }.ToList();
+            var data = ((IEnumerable< dynamic >) listdata).Where(x => x.Id == i).SingleOrDefault();
+            return data;
+        }
+    }
+
+}
